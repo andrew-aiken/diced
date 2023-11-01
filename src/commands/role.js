a957acd9c7b029bffcf633a95d2cfd6b7bdf7556
Andrew Aiken <andrew.aiken@machinemetrics.com>
2023-11-01 18:33:13 -0400

    simple dice roller

---
+++ src/commands/role.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('role')
+    .setDescription('Roles a custom dice')
+    .addStringOption(option =>
+      option
+        .setName('dice')
+        .setDescription('What type of dice to role')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const [numDice, numSides] = interaction.options.getString('dice').split('d').map(Number);
+    let total = 0;
+    let results = [];
+
+    if (isNaN(numDice) || isNaN(numSides) || numDice < 1 || numDice > 30 || numSides < 1 || numSides > 1001) {
+      await interaction.reply('Invalid input. Please enter a valid input in the format "NdS", where N is the number of dice and S is the number of sides.');
+      return;
+    } else {
+      console.log(`Rolling ${numDice}d${numSides}:`);
+      for (let i = 0; i < numDice; i++) {
+        const result = Math.floor(Math.random() * numSides) + 1;
+        console.log(`Die ${i + 1}: ${result}`);
+        total += result;
+        results.push(result);
+      }
+    }
+
+    await interaction.reply(`\`\`\`${total} - ${numDice}d${numSides} (${results.join(' ')})\`\`\``);
+  },
+};
